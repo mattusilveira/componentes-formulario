c9d9aeeb54d4989e9a9001b4ba53a563ebe9071b
mattusilveira <matheus12062007silveira7@gmail.com>
2024-07-19 11:35:11 -0300

    commit criação componentes formulario

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+import ProductList from '@/components/ProductList.vue'
+
+const produtos = ref([
+  { id: 1, nome: 'Produto 1', preco: 'R$ 20,50' },
+  { id: 2, nome: 'Produto 2', preco: 'R$ 120,50' },
+  { id: 3, nome: 'Produto 3', preco: 'R$ 220,50' },
+  { id: 4, nome: 'Produto 4', preco: 'R$ 320,50' },
+])
+
+function excluir(id) {
+  const index = produtos.value.findIndex(produto => produto.id === id)
+  produtos.value.splice(index, 1)
+}
+
+function excluirTodos() {
+  produtos.value = []
+}
+</script>
+
+<template>
+  <h1>Meus Produtos</h1>
+  <product-list :produtos="produtos" @excluir="excluir" @apagarTudo="excluirTodos" />
+</template>
+
+<style scoped></style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps(['produtos'])
+</script>
+
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Preço</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+
+        <body>
+            <tr v-for="produto in produtos" :key="produto.id">
+                <td>{{ produto.nome }}</td>
+                <td>{{ produto.preco }} </td>
+                <td><i class="mdi mdi-delete" @click="$emit('excluir', produto.id)" /></td>
+            </tr>
+        </body>
+    </table>
+    <button @click="$emit('apagarTudo')">Apagar tudo</button>
+</template>
+
+<style scoped>
+tr i {
+    cursor: pointer;
+}
+</style>
